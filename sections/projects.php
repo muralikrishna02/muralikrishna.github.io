748df30b8c42deee7caa68335a817494e5582ec1
Murali krishna Avula <37668819+muralikrishna02@users.noreply.github.com>
2024-06-03 13:43:57 -0400

    Create projects.php

---
+++ sections/projects.php
@@ -0,0 +1,8 @@
+<section id="projects" class="container py-5">
+    <div class="row">
+        <div class="col">
+            <h2>Projects</h2>
+            <p>Describe the projects you've worked on. You can include details about the technologies used, your role in the project, and any notable achievements or outcomes.</p>
+        </div>
+    </div>
+</section>
