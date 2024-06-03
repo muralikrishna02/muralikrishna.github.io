f4f0ce544484ffbfc5ad16ca09ea3f3b943b1c24
Murali krishna Avula <37668819+muralikrishna02@users.noreply.github.com>
2024-06-03 13:44:12 -0400

    Create contact.php

---
+++ sections/contact.php
@@ -0,0 +1,8 @@
+<section id="contact" class="container py-5">
+    <div class="row">
+        <div class="col">
+            <h2>Contact Me</h2>
+            <p>Provide a way for visitors to get in touch with you. You can include a contact form, your email address, social media links, or any other contact information you prefer.</p>
+        </div>
+    </div>
+</section>
