9618d3080817b685387a327bb03f82822ce0f79b
Murali krishna Avula <37668819+muralikrishna02@users.noreply.github.com>
2024-06-03 13:52:25 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-// script.js
-// Add your JavaScript code here
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
