4ddcf751ec9c1fad926cdd90508b24e8585c0b51
Murali krishna Avula <37668819+muralikrishna02@users.noreply.github.com>
2024-06-03 13:43:38 -0400

    Create experience.php

---
+++ sections/experience.php
@@ -0,0 +1,8 @@
+<section id="experience" class="container py-5">
+    <div class="row">
+        <div class="col">
+            <h2>Experience</h2>
+            <p>Add your work experience here. You can list your previous jobs, internships, freelance projects, or any relevant experiences you've had in the past.</p>
+        </div>
+    </div>
+</section>
