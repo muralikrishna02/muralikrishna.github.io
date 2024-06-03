3a59b6a633fc2a9f409475283d1d877ed98f29b3
Murali krishna Avula <37668819+muralikrishna02@users.noreply.github.com>
2024-06-03 13:53:42 -0400

    Update and rename about.php to about.html

--- sections/about.php
+++
@@ -1,8 +0,0 @@
-<section id="about" class="container py-5">
-    <div class="row">
-        <div class="col">
-            <h2>About Me</h2>
-            <p>Add some information about yourself here. You can write about your skills, experiences, interests, and anything else you think is relevant for potential employers.</p>
-        </div>
-    </div>
-</section>
