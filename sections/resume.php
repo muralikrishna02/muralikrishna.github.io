e75c5c2a4e97b288b64376772825b3c88c7e54b1
Murali krishna Avula <37668819+muralikrishna02@users.noreply.github.com>
2024-06-03 13:44:25 -0400

    Create resume.php

---
+++ sections/resume.php
@@ -0,0 +1,8 @@
+<section id="resume" class="container py-5">
+    <div class="row">
+        <div class="col">
+            <h2>Resume</h2>
+            <p>You can either include your resume directly on this page or provide a download link to your resume file (PDF, DOCX, etc.). Make sure your resume highlights your skills, experiences, and qualifications effectively.</p>
+        </div>
+    </div>
+</section>
